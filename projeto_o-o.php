a0c1f2169813b189f362f76315983eccfd7ae759
aprendiztriste <100315939+aprendiztriste@users.noreply.github.com>
2022-03-09 00:18:13 -0300

    Update projeto_o-o.php

--- projeto_o-o.php
+++ projeto_o-o.php
@@ -1,12 +1,12 @@
 <html>
 <body>
-<form action = "Exercicio2.php" method ="POST"></form>
+<form action = "Exercicio2.php" method ="POST">
 <label>fareheint</label>
 <input type="number" name"fareheint"/><br/><br/>
 <input type = "submit" value =">> graus celsius"/><hr>
 </body>
 <?php
-if(isset($_POST['fareheint']))
-$fareheint;
+if(isset($_POST['fareheint'])){
+$fareheint;}
 $celsius =[5*($fareheint-32)/9];
 echo"graus celsius:$celsius";
